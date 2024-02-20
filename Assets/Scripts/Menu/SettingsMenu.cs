b166c4f05e46e1adea371ddcbf574c41890a96a7
Chinodiazs <chinodiaz0701@gmail.com>
2024-02-19 22:15:13 -0600

    SettingMenu1.0

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -2,59 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-
-    public Dropdown Resolutionlist;
-
-    Resolution[] resolutions;
-
-    private void Start()
-    {
-        resolutions = Screen.resolutions;
-
-
-        Resolutionlist.ClearOptions();
-
-        List<string> options = new List<string>();
-
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        
-        Resolutionlist.AddOptions(options);
-        Resolutionlist.value = currentResolutionIndex;
-        Resolutionlist.RefreshShownValue();
-    }
-
-    public void SetResolution (int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
-
-    public void ReturnBotton()
+    public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
-
-
-    public void SetFullscreen(bool isFullscreen)
-    {
-
-        Screen.fullScreen = isFullscreen;
-
-    }
-
 }
