9e52b9d0e3e73dfac5ef903107e89740804ab173
Scar-46 <87331085+Scar-46@users.noreply.github.com>
2024-12-29 23:34:03 -0600

    Added new assets

---
+++ Assets/SetTrap.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Tilemaps;
+
+public class SetTrap : MonoBehaviour
+{
+    [SerializeField]
+    private float damage;
+
+
+    Animator _Animator;
+
+
+    private void Awake()
+    {
+        _Animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Step");
+        _Animator.SetTrigger("Step");
+
+        if (collider.transform.tag == "Enemy")
+        {
+            collider.GetComponent<EnemyHealth>().DealDamage(damage, Vector2.zero);
+        }
+
+        if (collider.transform.tag == "Player")
+        {
+            PlayerStats.Instance.DealDamage(damage);
+        }
+    }
+}
